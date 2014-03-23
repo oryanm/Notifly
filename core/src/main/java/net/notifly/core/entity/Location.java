ebd2b4d6b5bf5e5f9269b20a6e471bd67b87a9e3
oryanm <oryan.mat@gmail.com>
2014-03-23 21:29:03 +0200

    added stuff

--- core/src/main/java/net/notifly/core/entity/Location.java
+++ core/src/main/java/net/notifly/core/entity/Location.java
@@ -1,13 +1,43 @@
 package net.notifly.core.entity;
 
+import android.location.Address;
+
 public class Location
 {
-  double longitude;
-  double latitude;
+  private int id;
+  private double longitude;
+  private double latitude;
+
+  public Location(int id, double longitude, double latitude)
+  {
+    this.id = id;
+    this.longitude = longitude;
+    this.latitude = latitude;
+  }
 
   public Location(double longitude, double latitude)
   {
     this.longitude = longitude;
     this.latitude = latitude;
+  }
+
+  public static Location from(Address address)
+  {
+    return new Location(address.getLongitude(), address.getLatitude());
+  }
+
+  public int getId()
+  {
+    return id;
+  }
+
+  public double getLongitude()
+  {
+    return longitude;
+  }
+
+  public double getLatitude()
+  {
+    return latitude;
   }
 }
